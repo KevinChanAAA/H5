d98d81f2c377c5475ddeaaf36503a62542cd1b34
Kevin <409174556@qq.com>
2020-04-27 19:08:17 +0800

    init:repo

---
+++ src/Header/header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default class header extends React.Component{
+    render(){
+        return(
+            
+
+            
+            <div className="container">
+              <div className="icon">
+                  <a href="https://github.com/">
+                  <svg height="32" class="octicon octicon-mark-github text-white" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
+                      <g transform="translate(0,0)">
+                      <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                      </g>
+                      </svg>
+                  </a>
+                </div>
+                <div className="list">
+                <svg height="24" class="octicon octicon-three-bars text-white" viewBox="0 0 12 16" version="1.1" width="18" aria-hidden="true"><path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path></svg>
+                </div>
+                <div className="signup">
+               
+                </div>
+                <span className="bg"></span>
+                <div className="title">
+                    <h1 >Trending</h1>
+                       <p>             
+                         See what the GitHub community is most excited about today.       
+                        </p>            
+                 </div>                               
+
+                
+            </div>
+          
+           
+            
+        );
+    }
+}
+
+
